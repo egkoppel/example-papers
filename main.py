7e7c4e46d352a0dd509e18d76b5041bb74dab6e4
ronakye <ronakde84@gmail.com>
2025-03-08 14:29:49 +0000

    use thing5

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import PyPDF2
+import pypdf
 
 def extract_text_from_pdf(pdf_path):
     # Open the PDF file in binary mode
     with open(pdf_path, 'rb') as file:
         # Create a PDF reader object
-        reader = PyPDF2.PdfReader(file)
+        reader = pypdf.PdfReader(file)
         text = ""
         # Loop through all the pages and extract text
         for page in reader.pages:
@@ -14,8 +14,8 @@ def extract_text_from_pdf(pdf_path):
         return text
 
 # Example usage:
-pdf_file_path = 'databases_2425_1to8-B.pdf'  # Replace with your PDF file path
+pdf_file_path = 'L24fullnotesp2.pdf'  # Replace with your PDF file path
 pdf_text = extract_text_from_pdf(pdf_file_path)
-with open("thing3.txt", "w") as f:
+with open("thing5.txt", "w") as f:
     f.write(pdf_text)
 print(pdf_text)

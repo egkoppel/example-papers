8725d185582662c3484eb4d29fd8cbfe87ad011b
ronakye <ronakde84@gmail.com>
2025-03-08 22:00:12 +0000

    WOW

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import chatgpt
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    questions = chatgpt.question_summary()
-    return render_template("ui.html", questions=questions.questions)
+    print(request.files)
+    single_question = chatgpt.Question(
+        question_number="1",
+        question_content="What is the capital of France?",
+        topics=["Geography", "European Capitals"],
+        pages=[chatgpt.PageSection(start_page=57,end_page=58, file_search_citation="L24fullnotesp2.pdf")]  # Assuming no PageSection instances for this example
+    )
+    #questions = chatgpt.question_summary()
+    question_list = chatgpt.QuestionList(questions=[single_question])
+    return render_template("ui.html", questions=question_list.questions)
+

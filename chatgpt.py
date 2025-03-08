6e2fb7a073e4b92f5a5f31eb9e155b449306af3b
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-03-08 19:13:21 +0000

    Make the web thingy

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from pydantic_core import from_json
 
 load_dotenv()
 
@@ -15,7 +16,7 @@ class PageSection(BaseModel):
 
 class Question(BaseModel):
     question_number: str
-    question_name: str
+    question_content: str
     topics: list[str]
     pages: list[PageSection]
 
@@ -24,45 +25,56 @@ class QuestionList(BaseModel):
     questions: list[Question]
 
 
-assistant = client.beta.assistants.create(
-    name="Che Fixer",
-    instructions=f"You are a university tutor and have access to lecture notes through file_search. You will be given "
-                 f"questions a student is failing miserably at. You will be given text of questions, and should list "
-                 f"the relevant topics from the lecture notes as well as any relevant page numbers. You should output "
-                 f"ONLY a json file according to the following schema. ```json\n"
-                 f"{QuestionList.model_json_schema()}\n```",
-    tools=[{"type": "file_search"}],
-    model="gpt-4o-mini",
-)
-
-assistant = client.beta.assistants.update(
-    assistant_id=assistant.id,
-    tool_resources={"file_search": {"vector_store_ids": ["vs_67cc6233fb008191b683cbe63b091687"]}},
-)
-
-thread = client.beta.threads.create()
-
-with open("thing8.txt", "r") as f:
-    question_text = f.read()
-
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=f"Here is the list of questions:\n\n{question_text}",
-)
-
-run = client.beta.threads.runs.create_and_poll(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-)
-
-if run.status == 'completed':
-    messages = client.beta.threads.messages.list(
-        thread_id=thread.id
+def question_summary():
+    assistant = client.beta.assistants.create(
+        name="Che Fixer",
+        instructions=f"You are a university tutor and have access to lecture notes through file_search. You will be given "
+                     f"questions a student is failing miserably at. You will be given text of questions, and should list "
+                     f"the relevant topics from the lecture notes as well as any relevant page numbers. Extract the "
+                     f"question content, INCLUDING ANY SUBQUESTIONS, and convert any math into LaTeX."
+                     f"You should output "
+                     f"ONLY a json file according to the following schema. ```json\n"
+                     f"{QuestionList.model_json_schema()}\n```",
+        tools=[{"type": "file_search"}],
+        model="gpt-4o-mini",
     )
-    print(messages)
-else:
-    print(run.status)
-    print(run.last_error)
 
-    print(run)
+    assistant = client.beta.assistants.update(
+        assistant_id=assistant.id,
+        tool_resources={"file_search": {"vector_store_ids": ["vs_67cc6233fb008191b683cbe63b091687"]}},
+    )
+
+    thread = client.beta.threads.create()
+
+    with open("thing8.txt", "r") as f:
+        question_text = f.read()
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=f"Extract the topics and page numbers for EVERY question in the following text\n\n{question_text}",
+    )
+
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+
+    if run.status == 'completed':
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        data = messages.data[0].content[0].text.value
+        print(data)
+        data = data.split("```json\n")[1].split("\n```")[0]
+        result = QuestionList.model_validate(from_json(data))
+        print(repr(result))
+        return result
+    else:
+        print(run.status)
+        print(run.last_error)
+
+        print(run)
+
+if __name__ == "__main__":
+    question_summary()
--- web.py
+++ web.py
@@ -1,17 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+import chatgpt
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '''
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.min.mjs" type="module"></script>
-    <iframe id="pdf-js-viewer" src="/static/pdfjs-4/web/viewer.html?file=/static/L24fullnotesp2.pdf" title="webviewer" frameborder="0" style="width:50%;height:100%"></iframe>
-    <div style="display: inline-block; width:50%; float:rifgh">
-    <ul>
-    <li><a onclick="document.getElementById(&quot;pdf-js-viewer&quot;).contentWindow.PDFViewerApplication.page=1">Page 1</a></li>
-    <li><a onclick="document.getElementById(&quot;pdf-js-viewer&quot;).contentWindow.PDFViewerApplication.page=2">Page 2</a></li>
-    <li><a onclick="document.getElementById(&quot;pdf-js-viewer&quot;).contentWindow.PDFViewerApplication.page=3">Page 3</a></li>
-    </ul>
-    </div>
-    '''
+    questions = chatgpt.question_summary()
+    return render_template("ui.html", questions=questions.questions)

81fc6f56e233c5e6b823679767758779919ebb55
ronakye <ronakde84@gmail.com>
2025-03-08 14:33:33 +0000

    mathnotes woah

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def extract_text_from_pdf(pdf_path):
         reader = pypdf.PdfReader(file)
         text = ""
         # Loop through all the pages and extract text
-        for page in reader.pages:
+        for page_number,page in enumerate(reader.pages):
             page_text = page.extract_text()
             if page_text:  # Check if text is found on the page
-                text += page_text
+                text += f"\n========{page_number}======\n" + page_text
         return text
 
 # Example usage:
 pdf_file_path = 'L24fullnotesp2.pdf'  # Replace with your PDF file path
 pdf_text = extract_text_from_pdf(pdf_file_path)
-with open("thing5.txt", "w") as f:
+with open("Mathnotes.txt", "w") as f:
     f.write(pdf_text)
 print(pdf_text)

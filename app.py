5b64cd6cb58864c8afa356c4ffc04a13f760437d
FinleyStirk06 <FinleyStirk06@gmail.com>
2025-03-08 19:44:14 +0000

    Manim generation

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-import chatgpt
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    questions = chatgpt.question_summary()
-    return render_template("ui.html", questions=questions.questions)
